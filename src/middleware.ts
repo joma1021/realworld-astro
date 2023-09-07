195d81ae6b2c40ac01f53650e557a6b9c510bb04
mjost <marius.jost@inovex.de>
2023-09-07 12:48:14 +0200

    protect routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,16 @@
 import { defineMiddleware } from "astro:middleware";
+import { getUserSessionData } from "./services/session-service";
 
-export const onRequest = defineMiddleware(async (context, next) => {
-  //   TODO: protect routes here
+export const onRequest = defineMiddleware(async ({ cookies, url, redirect }, next) => {
+  const userSession = getUserSessionData(cookies);
+  const pathname = url.pathname;
+  console.log(pathname);
+  // Protect routes
+  if (!userSession.isLoggedIn) {
+    if (pathname === "/settings") return redirect("/register");
+    if (pathname === "/editor") return redirect("/register");
+    if (pathname.includes("/editor/")) return redirect("/register");
+  }
 
   return next();
 });
