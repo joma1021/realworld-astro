92d61a2b6c18f6d365c5e1c391a35df2fdfc14d8
mjost <marius.jost@inovex.de>
2023-09-14 15:43:29 +0200

    make article static + update buttons

--- src/components/buttons/DeleteButton.tsx
+++
@@ -1,9 +0,0 @@
-export default function DeleteButton() {
-  return (
-    <form style={{ display: "inline-block" }} method="post">
-      <button class="btn btn-sm btn-outline-danger" type="submit" name="action" value="DELETE">
-        <i class="ion-trash-a"></i> Delete Article
-      </button>
-    </form>
-  );
-}
